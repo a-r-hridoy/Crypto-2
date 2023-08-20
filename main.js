90b0a6dd5a016198baf106962b6b590fd2b46a25
unknown <arhridoy@gmail.com>
2023-08-20 18:59:31 +0600

    change-style

--- main.js
+++ main.js
@@ -30,6 +30,28 @@ $('.sponsors').owlCarousel({
     }
     
 });
+
+
+$('.testimonial').owlCarousel({
+    loop:true,
+    margin:10,
+    nav:false,
+    responsive:{
+        0:{
+            items:1
+        },
+        600:{
+            items:2
+        },
+        1000:{
+            items:3
+        }
+    }
+})
+
+
+
+
 $('.country').owlCarousel({
     items:5,
     loop:true,
@@ -52,6 +74,8 @@ $('.country').owlCarousel({
 });
 
 // Carousel area
+
+
 
 $(document).ready(function () {
     // code from https://code.mukto.info/counter-up-with-a-simple-jquery/
